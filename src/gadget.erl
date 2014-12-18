21d3f4676d07f82404bec638b6005fd9150d39cc
Brujo Benavides <elbrujohalcon@gmail.com>
2014-12-18 14:09:04 -0300

    Cosmetics

--- src/gadget.erl
+++ src/gadget.erl
@@ -12,53 +12,55 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec start(application:start_type(), term()) ->
-    {ok, pid()} | {ok, pid(), term()} | {error, term()}.
+  {ok, pid()} | {ok, pid(), term()} | {error, term()}.
 start(_StartType, _StartArgs) ->
-    {ok, Pid} = gadget_sup:start_link(),
-    start_cowboy_listeners(),
-    {ok, Pid}.
+  {ok, Pid} = gadget_sup:start_link(),
+  start_cowboy_listeners(),
+  {ok, Pid}.
 
 -spec stop(term()) -> ok.
 stop(_State) ->
-    cowboy:stop_listener(http_gadget),
-    ok.
+  cowboy:stop_listener(http_gadget),
+  ok.
 
-%% Shell start function
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% External functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec start() -> ok.
 start() ->
-    application:ensure_all_started(gadget).
+  application:ensure_all_started(gadget).
+
+-spec webhook(atom(), map()) -> ok | {error, term()}.
+webhook(elvis, RequestMap) ->
+  elvis:webhook(RequestMap).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Not exported functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 start_cowboy_listeners() ->
   {ok, Port} = application:get_env(cowboy, http_port),
   {ok, ListenerCount} = application:get_env(cowboy, http_listener_count),
   Dispatch = cowboy_router:compile([
-    {'_', [
-            %% Web UI
-            {"/", gadget_plain_dtl_handler, [index_dtl]},
-            {"/repos", gadget_repos_handler, []},
-            {"/active-tools", gadget_on_handler, []},
-            {"/active-tools/:tool", gadget_off_handler, []},
-            {"/assets/[...]", cowboy_static, {dir, "assets"}},
+    {'_', [ %% Web UI
+            {"/", gadget_plain_dtl_handler, [index_dtl]}
+          , {"/repos", gadget_repos_handler, []}
+          , {"/active-tools", gadget_on_handler, []}
+          , {"/active-tools/:tool", gadget_off_handler, []}
+          , {"/assets/[...]", cowboy_static, {dir, "assets"}}
             %% OAuth
-            {"/login", gadget_login_handler, []},
-            {"/callback", gadget_callback_handler, []},
+          , {"/login", gadget_login_handler, []}
+          , {"/callback", gadget_callback_handler, []}
             %% Webhook
-            {"/webhook/:tool", gadget_webhook_handler, []}
+          , {"/webhook/:tool", gadget_webhook_handler, []}
           ]
     }
   ]),
-  RanchOpts =
-      [
-        {port, Port}
-      ],
+  RanchOpts = [{port, Port}],
   CowboyOpts =
-      [
-       {env,       [{dispatch, Dispatch}]},
-       {compress,  true},
-       {timeout,   12000}
-      ],
+    [ {env,       [{dispatch, Dispatch}]}
+    , {compress,  true}
+    , {timeout,   12000}
+    ],
   cowboy:start_http(http_gadget, ListenerCount, RanchOpts, CowboyOpts).
-
--spec webhook(atom(), map()) -> ok | {error, term()}.
-webhook(elvis, RequestMap) -> elvis:webhook(RequestMap).
--- src/tools/gadget_elvis.erl
+++ src/tools/gadget_elvis.erl
@@ -11,12 +11,13 @@
 -spec on(term(), term(), term(), term()) -> ok.
 on(Repo, Cred, Events, ToolName) ->
   {ok, RepoInfo} = egithub:repo(Cred, Repo),
-  ok = case RepoInfo of
-           #{<<"private">> := true} ->
-               add_user(Cred, RepoInfo, get_username(ToolName));
-           _ ->
-               ok
-       end,
+  ok =
+    case RepoInfo of
+      #{<<"private">> := true} ->
+        add_user(Cred, RepoInfo, get_username(ToolName));
+      _ ->
+        ok
+   end,
 
   WebhookUrl = elvis_utils:to_str(get_url(ToolName)),
   {ok, _Hook} = egithub:create_webhook(Cred, Repo, WebhookUrl, Events).
@@ -25,26 +26,25 @@ on(Repo, Cred, Events, ToolName) ->
 %%% Private
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 add_user(Cred, RepoInfo, Username) ->
-    #{<<"full_name">> := Repo,
-      <<"owner">> := Owner} = RepoInfo,
-    #{<<"type">> := Type,
-      <<"login">> := Login} = Owner,
+  #{ <<"full_name">> := Repo
+   , <<"owner">> := Owner
+   } = RepoInfo,
+  #{ <<"type">> := Type
+   , <<"login">> := Login} = Owner,
 
-    case Type of
-        <<"User">> ->
-            egithub:add_collaborator(Cred, Repo, Username);
-        _Org ->
-            add_user_to_org(Cred, Login, Repo, Username)
-    end.
+  case Type of
+    <<"User">> -> egithub:add_collaborator(Cred, Repo, Username);
+    _Org -> add_user_to_org(Cred, Login, Repo, Username)
+  end.
 
 add_user_to_org(Cred, Org, Repo, Username) ->
-    {ok, _} = egithub:create_team(Cred, Org, "Services", "pull", [Repo]),
-    {ok, Teams} = egithub:teams(Cred, Org),
-    [TeamId] = [TeamId
-                || #{<<"id">> := TeamId, <<"name">> := Name} <- Teams,
-                   <<"Services">> == Name],
-    ok = egithub:add_team_repository(Cred, TeamId, Repo),
-    egithub:add_team_member(Cred, TeamId, Username).
+  {ok, _} = egithub:create_team(Cred, Org, "Services", "pull", [Repo]),
+  {ok, Teams} = egithub:teams(Cred, Org),
+  [TeamId] = [TeamId
+              || #{<<"id">> := TeamId, <<"name">> := Name} <- Teams,
+                 <<"Services">> == Name],
+  ok = egithub:add_team_repository(Cred, TeamId, Repo),
+  egithub:add_team_member(Cred, TeamId, Username).
 
 get_username(ToolName) ->
   {ok, WebhookMap} = application:get_env(gadget, webhooks),
