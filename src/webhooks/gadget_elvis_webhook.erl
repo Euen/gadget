212716c977ed08e536bf59a988a808480439e683
Brujo Benavides <elbrujohalcon@gmail.com>
2015-04-08 18:25:33 -0300

    Unneeded function removed

--- src/webhooks/gadget_elvis_webhook.erl
+++ src/webhooks/gadget_elvis_webhook.erl
@@ -2,15 +2,7 @@
 
 -behaviour(egithub_webhook).
 
--export([event/2]).
 -export([handle_pull_request/3]).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% External Functions
-
--spec event(egithub:credentials(), egithub_webhook:request()) ->
-  ok | {error, term()}.
-event(Cred, Request) -> egithub_webhook:event(?MODULE, Cred, Request).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Callbacks
