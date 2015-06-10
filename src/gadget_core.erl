3b70afa990d671bf6357908f2dc81ad5680910a1
David Cao <david_bluetrain@hotmail.com>
2015-06-10 16:21:36 -0300

    [#63] Add Decoupling gadget_repos_handler:handle() code to gadget_core:repositories()

--- src/gadget_core.erl
+++ src/gadget_core.erl
@@ -1,9 +1,10 @@
 %%% @doc Main application module
 -module(gadget_core).
 
--export([ register/3
-        , unregister/3
-        ]).
+-export([register/3]).
+-export([unregister/3]).
+-export([repositories/1]).
+-export([repo_info/2]).
 
 %% @doc registers a repo for processing
 -spec register(string(), atom(), string()) -> gadget_repos:repo().
@@ -43,6 +44,44 @@ unregister(Repo, Tool, Token) ->
       [Id] -> egithub:delete_webhook(Cred, Repo, Id)
     end,
   gadget_repos_repo:unregister(Repo, Tool).
+
+-spec repositories(egithub:credentials()) -> list().
+repositories(Cred) ->
+  Opts = #{type => <<"owner">>},
+  {ok, Repos} = egithub:all_repos(Cred, Opts),
+  {ok, Orgs} = egithub:orgs(Cred),
+
+  OrgReposFun =
+    fun(#{<<"login">> := OrgName}) ->
+      {ok, OrgRepos} = egithub:all_org_repos(Cred, OrgName, Opts),
+      OrgRepos
+    end,
+  AllOrgsRepos = lists:flatmap(OrgReposFun, Orgs),
+
+  PublicRepos =
+    [Repo || Repo <- Repos ++ AllOrgsRepos,
+             gadget_utils:is_admin(Repo), gadget_utils:is_public(Repo)],
+
+  lists:sort([repo_info(Cred, Repo) || Repo <- PublicRepos]).
+
+-spec repo_info(egithub:credentials(), string()) -> [tuple()].
+repo_info(Cred, Repo) ->
+  Name = maps:get(<<"name">>, Repo),
+  FullName = maps:get(<<"full_name">>, Repo),
+  HtmlUrl = maps:get(<<"html_url">>, Repo),
+  Hooks =
+    case egithub:hooks(Cred, FullName) of
+      {ok, HooksResult} -> HooksResult;
+      {error, _} -> []
+    end,
+
+  Status = gadget_utils:active_tools(Hooks),
+  StatusList = lists:map(fun maps:to_list/1, Status),
+  [ {full_name, FullName}
+  , {name, Name}
+  , {html_url, HtmlUrl}
+  , {status, StatusList}
+  ].
 
 -spec check_result(term()) -> term().
 check_result({error, {"422", _, _}}) ->
