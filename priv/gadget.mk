4c3c98d3b0e7fb7f3027fec35d0a35ccc6073704
Brujo Benavides <elbrujohalcon@gmail.com>
2015-09-07 15:28:14 -0300

    [Fix #128] Dialyzer almost always fails

--- priv/gadget.mk
+++ priv/gadget.mk
@@ -8,4 +8,5 @@ else
 gadget-dialyze: $(DIALYZER_PLT)
 endif
 	$(gen_verbose) dialyzer --no_native --no_check_plt --raw --quiet \
-		$(DIALYZER_OPTS/--verbose/) $(DIALYZER_DIRS) > gadget-dialyze.result
+		$(DIALYZER_OPTS/--verbose/) $(DIALYZER_DIRS) > gadget-dialyze.result; \
+		echo "ok"
