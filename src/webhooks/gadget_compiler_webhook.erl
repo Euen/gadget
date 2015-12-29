c8f476e0793e54b2ad0cfd15bc1abb2e281791b1
Harenson Henao <harenson@gmail.com>
2015-12-29 15:19:37 -0500

    [#138] Reestructure code

--- src/webhooks/gadget_compiler_webhook.erl
+++ src/webhooks/gadget_compiler_webhook.erl
@@ -42,16 +42,16 @@ process_pull_request(RepoDir, RepoName, Branch, GitUrl, GithubFiles) ->
     {ok, Messages}
   catch
     _:{error, {status, ExitStatus, Output}} ->
-      Lines = output_to_lines(Output),
-      case is_compiler_error(Lines) of
-        false -> {error, {failed, ExitStatus}};
-        true ->
+      Lines = gadget_utils:output_to_lines(Output),
+      case error_source (Lines) of
+        unknown -> {error, {failed, ExitStatus}};
+        compiler ->
           Comments1 = extract_errors(Lines),
           Messages1 =
             gadget_utils:messages_from_comments("Compiler",
                                                 Comments1,
                                                 GithubFiles),
-          {ok, Messages1}
+          report_compiler_error(Messages1, ExitStatus)
       end;
     _:Error ->
       lager:warning(
@@ -88,15 +88,27 @@ extract_errors([Line|Lines], Regex, Errors) ->
     end,
   extract_errors(Lines, Regex, NewErrors).
 
-output_to_lines(Output) ->
-  DecodedOutput = unicode:characters_to_binary(Output),
-  try
-    re:split(DecodedOutput, "\n", [{return, binary}, trim])
-  catch
-    _:Error ->
-      lager:warning("Uncomprehensible output: ~p", [DecodedOutput]),
-      Error
+error_source(Lines) ->
+  LastLines = lists:sublist(lists:reverse(Lines), 3),
+  Regexes = ["make[:] [*][*][*] [[][^]]*[]] Error",
+             "ERROR[:] compile failed",
+             "Compiling .* failed$"],
+  MatchesRegexes =
+    fun(Line) ->
+      lists:any(fun(Regex) -> nomatch /= re:run(Line, Regex) end, Regexes)
+    end,
+  case lists:any(MatchesRegexes, LastLines) of
+    true -> compiler;
+    false -> unknown
   end.
 
-is_compiler_error(Lines) ->
-  <<"make: *** [app] Error 2">> /= lists:last(Lines).
+report_compiler_error([], ExitStatus) -> {error, {failed, ExitStatus}};
+report_compiler_error([#{commit_id := CommitId} | _] = Messages, ExitStatus) ->
+  ExtraMessage =
+    #{commit_id => CommitId,
+      path      => "",
+      position  => 0,
+      text      => <<"**Compiler** failed with exit status: ",
+                     (integer_to_binary(ExitStatus))/binary>>
+     },
+  {ok, [ExtraMessage | Messages]}.
