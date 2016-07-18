09a68d35375c8f0cfbde041cef9fd2cf8c5a254d
Euen <euenlopez@gmail.com>
2016-07-18 13:48:36 -0300

    mend

--- src/webhooks/gadget_lewis_webhook.erl
+++ src/webhooks/gadget_lewis_webhook.erl
@@ -76,7 +76,7 @@ create_local_properties(RepoDir) ->
   AndroidSDK = os:getenv("ANDROID_HOME"),
   LocalPropPath = filename:join(RepoDir, "local.properties"),
   LocalPropData = ["sdk.dir=" , AndroidSDK],
-  ok = file:write_file(LocalPropPath, [LocalPropData, $\n])).
+  ok = file:write_file(LocalPropPath, [LocalPropData, $\n]).
 
 run_lewis(RepoDir) ->
   Command = ["cd ", RepoDir, "; ", "./gradlew lint --stacktrace"],
