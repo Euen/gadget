3d25a74a461d2eeb1f6cf7137e573ec0f07c7b50
Euen <euenlopez@gmail.com>
2016-11-02 18:50:28 -0300

    [Close #276] add elvis_core dependency

--- build/install_quickcheck.escript
+++
@@ -1,4 +0,0 @@
-%% -*- erlang -*-
-%%! -smp enable -sname factorial -mnesia debug verbose
-main([]) ->
-  eqc_install:install().
--- build/install_quickcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /tmp
-wget http://www.quviq.com/downloads/eqc-sf-trial2016.zip
-unzip eqc-sf-trial2016.zip
-cd "Quviq QuickCheck version 1.37.2"
-mv /tmp/install_quickcheck.escript .
-/usr/local/bin/escript install_quickcheck.escript
