41101369b8f95848eea3686a79137fc030a8578a
Ignacio Mendizabal <ignacio@inakanetworks.com>
2017-05-12 16:17:18 -0300

    updating quickcheck version

--- build/install_quickcheck.sh
+++ build/install_quickcheck.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /tmp
-wget http://www.quviq.com/downloads/eqc-sf-trial2016.zip
-unzip eqc-sf-trial2016.zip
-cd "Quviq QuickCheck version 1.37.2"
+wget http://quviq-licencer.com/downloads/eqcR19.zip
+unzip eqcR19.zip
+cd "Quviq QuickCheck version 1.40.2"
 mv /tmp/install_quickcheck.escript .
 /usr/local/bin/escript install_quickcheck.escript
